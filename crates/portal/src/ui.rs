bd4ed39e79bc4541f956ab7f399c884c161e7fc0
alimulap <mohammad4limul@gmail.com>
2025-01-27 15:10:32 +0700

    feat: input field label

--- crates/portal/src/ui.rs
+++ crates/portal/src/ui.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use bevy::{color::palettes::css::WHITE, prelude::*};
+use bevy::{color::palettes::css::WHITE, ecs::observer::TriggerTargets, prelude::*};
 use bevy_simple_text_input::{
     TextInput, TextInputPlugin, TextInputSettings, TextInputTextFont, TextInputValue,
 };
@@ -39,6 +39,7 @@ fn build_ui(mut cmd: Commands) {
                 parent.spawn(TextUI::new("Kocag"));
                 parent.spawn((
                     InputField,
+                    InputFieldLabel::new("Kocag"),
                     InputUInitialValue("1".into()),
                     InputFieldType::F32,
                 ));
@@ -59,7 +60,7 @@ fn build_ui(mut cmd: Commands) {
 }
 
 #[derive(Component)]
-#[require(Node, PanelTitle, PanelMaxWidth)]
+#[require(Node, PanelTitle)]
 struct Panel;
 
 #[derive(Component, Default)]
@@ -72,20 +73,18 @@ impl PanelTitle {
 }
 
 #[derive(Component)]
-struct PanelMaxWidth(Val);
-
-impl Default for PanelMaxWidth {
-    fn default() -> Self {
-        Self(Val::Vw(30.))
-    }
-}
+struct MaxWidth(Val);
 
 fn create_panel(
     trigger: Trigger<OnAdd, Panel>,
     mut cmd: Commands,
-    added_panel: Query<(&PanelTitle, &PanelMaxWidth)>,
+    title: Query<&PanelTitle>,
+    max_width: Query<&MaxWidth>,
 ) {
-    let (PanelTitle(title), PanelMaxWidth(width)) = added_panel.get(trigger.entity()).unwrap();
+    let PanelTitle(title) = title.get(trigger.entity()).unwrap();
+    let MaxWidth(width) = max_width
+        .get(trigger.entity())
+        .unwrap_or(&MaxWidth(Val::Vw(30.)));
     let title = cmd
         .spawn((TextUI::new(title), TextLayout {
             justify: JustifyText::Center,
@@ -120,7 +119,7 @@ fn create_panel(
         .insert_children(0, &[title, separator]);
 }
 
-#[derive(Component)]
+#[derive(Component, Clone)]
 #[require(Node, Text)]
 struct TextUI(String);
 
@@ -139,17 +138,24 @@ fn create_text_ui(trigger: Trigger<OnAdd, TextUI>, mut cmd: Commands, text_ui: Q
 #[derive(Component)]
 struct InputUI;
 
-#[derive(Component, Default)]
+#[derive(Component, Default, Clone)]
 struct InputUInitialValue(String);
 
 fn create_input_ui(
     trigger: Trigger<OnAdd, InputUI>,
     mut cmd: Commands,
-    input_ui: Query<&InputUInitialValue>,
+    // input_value: Query<&Parent, With<InputUI>>,
+    init_value: Query<&InputUInitialValue>,
+    max_width: Query<&MaxWidth>,
 ) {
-    let InputUInitialValue(value) = input_ui.get(trigger.entity()).unwrap();
+    let InputUInitialValue(value) = init_value.get(trigger.entity()).unwrap();
+    let MaxWidth(width) = max_width
+        .get(trigger.entity())
+        .unwrap_or(&MaxWidth(Val::Px(45.)));
     cmd.entity(trigger.entity()).insert((
         Node {
+            width: *width,
+            max_width: *width,
             border: UiRect::all(Val::Px(1.)),
             ..default()
         },
@@ -180,6 +186,8 @@ enum InputFieldType {
     F32,
 }
 
+type InputFieldLabel = TextUI;
+
 #[derive(Component)]
 struct InputFieldOldValue(String);
 
@@ -187,24 +195,38 @@ fn create_input_field(
     trigger: Trigger<OnAdd, InputField>,
     mut cmd: Commands,
     init_value: Query<&InputUInitialValue>,
+    label: Query<&InputFieldLabel>,
 ) {
     let init_value = init_value.get(trigger.entity()).unwrap();
+    cmd.entity(trigger.entity()).remove::<InputUInitialValue>();
+    let label = label.get(trigger.entity()).unwrap();
     cmd.entity(trigger.entity())
-        .insert((InputUI, InputFieldOldValue(init_value.0.clone())));
+        .insert((Node {
+            flex_direction: FlexDirection::Row,
+            ..default()
+        },))
+        .with_children(|parent| {
+            parent.spawn((label.clone(), Node {
+                margin: UiRect::right(Val::Px(3.)),
+                ..default()
+            }));
+            parent.spawn((
+                InputUI,
+                init_value.clone(),
+                InputFieldOldValue(init_value.0.clone()),
+            ));
+        });
 }
 
-#[allow(clippy::type_complexity)]
 fn input_field_validation_system(
     mut input_field: Query<
-        (
-            Mut<TextInputValue>,
-            &mut InputFieldOldValue,
-            &InputFieldType,
-        ),
-        (With<InputField>, Changed<TextInputValue>),
+        (&Parent, Mut<TextInputValue>, &mut InputFieldOldValue),
+        Changed<TextInputValue>,
     >,
+    input_type: Query<&InputFieldType>,
 ) {
-    for (mut value, mut old, input_type) in input_field.iter_mut() {
+    for (parent, mut value, mut old) in input_field.iter_mut() {
+        let input_type = input_type.get(parent.entities()[0]).unwrap();
         if match input_type {
             InputFieldType::String => true,
             InputFieldType::I32 => value.0.parse::<i32>().is_ok(),
