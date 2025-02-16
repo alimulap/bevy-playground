4784aeed5411d2aefbcfe354f3d188ba5e0d813d
Mohammad Alimul Adin Pramono <113952311+alimulap@users.noreply.github.com>
2025-02-16 17:58:22 +0700

    spawn block with position

--- crates/simple-space/src/modules/block.rs
+++ crates/simple-space/src/modules/block.rs
@@ -2,6 +2,8 @@ use avian2d::prelude::*;
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 
+use crate::constant;
+
 use super::{
     object_pool::{ObjectPool, PoolMarker},
     template::{Template, TemplateExt},
@@ -18,8 +20,8 @@ impl Plugin for BlockPlugin {
 fn setup(mut cmd: Commands) {
     let mut pool = ObjectPool::<Block>::new();
 
-    for _ in 0..1 {
-        pool.put(cmd.template::<Block>(true));
+    for _ in 0..20 {
+        pool.put(cmd.template::<Block>(BlockProp::inactive(Vec2::ZERO)));
     }
 
     cmd.insert_resource(pool);
@@ -31,28 +33,49 @@ pub struct Block;
 
 impl PoolMarker for Block {}
 
+struct BlockProp {
+    active: bool,
+    position: Vec2,
+}
+
+impl BlockProp {
+    fn active(position: Vec2) -> Self {
+        Self {
+            active: true,
+            position,
+        }
+    }
+
+    fn inactive(position: Vec2) -> Self {
+        Self {
+            active: false,
+            position,
+        }
+    }
+}
+
 impl Template for Block {
-    type Prop = bool;
-    fn construct(cmd: &mut Commands, active: Self::Prop) -> Entity {
+    type Prop = BlockProp;
+    fn construct(cmd: &mut Commands, prop: Self::Prop) -> Entity {
         let mut entt = cmd.spawn((
             Block,
             ShapeBundle {
                 path: GeometryBuilder::build_as(&shapes::Rectangle {
-                    extents: Vec2::new(50., 50.),
+                    extents: Vec2::new(constant::BLOCK_SIZE, constant::BLOCK_SIZE),
                     origin: shapes::RectangleOrigin::Center,
                     radii: None,
                 }),
-                transform: Transform::from_xyz(0., 300., 0.),
+                transform: Transform::from_translation(Vec3::from((prop.position, 0.))),
                 ..default()
             },
             Fill::color(Color::WHITE.with_alpha(0.)),
             Stroke::new(Color::WHITE, 3.),
-            Collider::rectangle(50., 50.),
+            Collider::rectangle(constant::BLOCK_SIZE, constant::BLOCK_SIZE),
             RigidBody::Static,
             SweptCcd::default(),
         ));
 
-        if !active {
+        if !prop.active {
             entt.insert((Visibility::Hidden, ColliderDisabled));
         }
 
